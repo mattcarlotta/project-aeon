9b7005f6fc3db17b0877f712f2e312ce98faf5f7
Matt <carlotta.matt@gmail.com>
2020-01-07 17:24:35 -0800

    Add requireAuth strategy

--- strategies/index.js
+++ strategies/index.js
@@ -3,4 +3,4 @@
 export { default as checkAuth } from "./checkAuth";
 export { default as localLogin } from "./localLogin";
 export { default as localSignup } from "./localSignup";
-// export { default as requireAuth } from "./requireAuth";
+export { default as requireAuth } from "./requireAuth";
