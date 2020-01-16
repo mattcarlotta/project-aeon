e21baf420fcd1fa9b4a00db0e54523dbea862157
Matt <carlotta.matt@gmail.com>
2020-01-16 09:44:58 -0800

    Use publicRunTimeConfig to persist db connection

--- src/database/index.js
+++ src/database/index.js
@@ -2,26 +2,21 @@ const promise = require("bluebird");
 const monitor = require("pg-monitor");
 const pgPromise = require("pg-promise");
 
-const initOptions = { promiseLib: promise }; // Database options
+const initOptions = { promiseLib: promise, noWarnings: false }; // Database options
 const pgp = pgPromise(initOptions); // initialize pg-promise w/options
 
 const { NODE_ENV, DB, DBHOST, DBPASSWORD, DBPORT, DBUSER } = process.env; //
 
-const dbConnection = () => {
-	if (NODE_ENV === "development" && monitor.isAttached()) {
-		monitor.detach();
-		monitor.attach(initOptions, ["query", "error"]);
-	} else {
-		monitor.attach(initOptions, ["query", "error"]);
-	}
+if (NODE_ENV === "development") {
+	monitor.attach(initOptions, ["query", "error"]);
+} else {
+	monitor.attach(initOptions, ["error"]);
+}
 
-	return pgp({
-		database: DB,
-		host: DBHOST,
-		password: DBPASSWORD,
-		port: DBPORT,
-		user: DBUSER,
-	});
-};
-
-module.exports = dbConnection();
+module.exports = pgp({
+	database: DB,
+	host: DBHOST,
+	password: DBPASSWORD,
+	port: DBPORT,
+	user: DBUSER,
+});
--- src/strategies/requireAuth.js
+++ src/strategies/requireAuth.js
@@ -1,7 +1,9 @@
-import db from "~database";
+import getConfig from "next/config";
 import { findUserById } from "~database/queries";
 import { parseSession, sendError } from "~utils/helpers";
 import { badCredentials } from "~utils/errors";
+
+const { db } = getConfig().publicRuntimeConfig;
 
 /**
  * Middleware function to check if a user is logged into a session and the session is valid.
@@ -15,6 +17,8 @@ export default next => async (req, res) => {
 
 	const existingUser = await db.oneOrNone(findUserById, [id]);
 	if (!existingUser) return sendError(badCredentials, res);
+
+	req.user = existingUser;
 
 	next(req, res);
 };
