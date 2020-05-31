6fbce8d573ffeef31a28f9789547bc7f2976d878
Matt <carlotta.matt@gmail.com>
2020-05-30 19:21:35 -0700

    Begin moving away from antd

--- src/components/Forms/TextArea/index.js
+++ src/components/Forms/TextArea/index.js
@@ -1,21 +1,22 @@
-import styled from "styled-components";
+import React from "react";
 import PropTypes from "prop-types";
+import styled from "styled-components";
 import Label from "~components/Forms/Label";
 import Errors from "~components/Forms/Errors";
 import ClickHandler from "~components/Forms/ClickHandler";
 
-const Input = ({
+const TextArea = ({
 	className,
 	containerStyle,
-	errors,
 	disabled,
-	inputStyle,
+	errors,
 	label,
 	name,
-	onChange,
 	placeholder,
+	onChange,
 	readOnly,
-	type,
+	rows,
+	style,
 	tooltip,
 	value
 }) => (
@@ -34,19 +35,20 @@ const Input = ({
 					{label && (
 						<Label name={name} label={label} tooltip={tooltip} htmlFor={name} />
 					)}
-					<input
+					<textarea
 						aria-label={name}
-						tabIndex={0}
-						type={type}
+						data-testid={name}
+						disabled={disabled}
 						name={name}
-						onBlur={handleBlur}
 						onChange={onChange}
 						onFocus={handleFocus}
+						onBlur={handleBlur}
 						placeholder={placeholder}
-						value={value}
-						style={inputStyle}
-						disabled={disabled}
 						readOnly={readOnly}
+						rows={rows || 10}
+						style={style}
+						tabIndex={0}
+						value={value}
 					/>
 					{errors && <Errors>{errors}</Errors>}
 				</div>
@@ -55,86 +57,55 @@ const Input = ({
 	</div>
 );
 
-Input.propTypes = {
+TextArea.propTypes = {
 	className: PropTypes.string.isRequired,
 	containerStyle: PropTypes.objectOf(
 		PropTypes.oneOfType([PropTypes.string, PropTypes.number])
 	),
 	disabled: PropTypes.bool,
 	errors: PropTypes.string,
-	inputStyle: PropTypes.objectOf(
+	onChange: PropTypes.func.isRequired,
+	name: PropTypes.string.isRequired,
+	placeholder: PropTypes.string,
+	label: PropTypes.string.isRequired,
+	readOnly: PropTypes.bool,
+	rows: PropTypes.number,
+	style: PropTypes.objectOf(
 		PropTypes.oneOfType([PropTypes.string, PropTypes.number])
 	),
-	label: PropTypes.string,
-	name: PropTypes.string,
-	onChange: PropTypes.func,
-	placeholder: PropTypes.string,
-	readOnly: PropTypes.bool,
 	tooltip: PropTypes.string,
-	type: PropTypes.string,
 	value: PropTypes.string
 };
 
-Input.defaultProps = {
-	disabled: false,
-	readOnly: false
-};
+export default styled(TextArea)`
+	@media (max-width: 768px) {
+		display: block !important;
+		width: 100% !important;
+	}
+	min-height: 230px;
+	padding: 0 10px;
+	display: flex;
+	flex-direction: column;
+	margin-bottom: 15px;
 
-export default styled(Input)`
-	position: relative;
-	display: inline-block;
-	height: 100px;
-	width: 100%;
-
-	input {
-		position: relative;
-		padding: 11px;
+	textarea {
+		padding: 10px;
+		height: 173px;
+		overflow-y: auto;
 		width: 100%;
-		font-size: 16px;
 		background: #fff;
-		border: 1px solid #e5e5e5;
+		color: #3a3a3a;
+		border: 1px solid ${({ errors }) => (errors ? "#d03916" : "#d3d3d3")};
 		border-radius: 4px;
-		transition: border 0.2s ease-in-out;
-
-		&:hover {
-			border: 1px solid #40a9ff;
-		}
-
+		transition: 0.2s ease-in-out;
+		transition-property: color, border;
+		resize: none;
 		&::placeholder {
-			color: #ccc;
+			color: #bbb;
 		}
-
 		&:focus {
 			outline: 0;
-		}
-	}
-
-	.focused {
-		svg {
-			color: #1e90ff;
-		}
-
-		input {
-			border: 1px solid #1e90ff;
-		}
-	}
-
-	.error {
-		input {
-			border: 1px solid #d14023 !important;
-		}
-	}
-
-	.disabled {
-		input {
-			cursor: not-allowed;
-			color: rgba(0, 0, 0, 0.25);
-			background: #f5f5f5;
-			border: 1px solid #e6d8d8;
-
-			&:hover {
-				border: 1px solid #e6d8d8;
-			}
+			border: 1px solid #1890ff;
 		}
 	}
 `;
